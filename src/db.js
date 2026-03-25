c0bf9f8ec3aa8d20a94ee73869adb017d1a96715
FinEredium1 <fahmed71@gatech.edu>
2026-03-25 14:10:21 -0400

    Changed ssl logic

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ if (!process.env.DATABASE_URL) {
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  //ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: true } : false,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: true } : false,
 });
 
 pool.query('SELECT 1').then(() => {
